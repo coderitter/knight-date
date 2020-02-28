1212badfb611544debf822f73a80ca2cf5f7f0ce
R Gastmeier <r.gastmeier@gmail.com>
2020-02-28 14:16:17 +0100

    Add utc and toIsoStringWithTimezone

---
+++ src/date.ts
@@ -0,0 +1,23 @@
+export function utc(date: Date = new Date): Date {
+  let utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds())
+  return new Date(utc)
+}
+
+export function toIsoStringWithTimezone(date: Date): string {
+  let tzo = -date.getTimezoneOffset()
+  let dif = tzo >= 0 ? '+' : '-'
+
+  return date.getFullYear() +
+    '-' + pad(date.getMonth() + 1) +
+    '-' + pad(date.getDate()) +
+    'T' + pad(date.getHours()) +
+    ':' + pad(date.getMinutes()) +
+    ':' + pad(date.getSeconds()) +
+    dif + pad(tzo / 60) +
+    ':' + pad(tzo % 60)
+}
+
+function pad(num: number) {
+  var norm = Math.floor(Math.abs(num))
+  return (norm < 10 ? '0' : '') + norm
+}
