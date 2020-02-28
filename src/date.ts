9224c4aeacc24cb24a3586bcffbbfe1d67461804
R Gastmeier <r.gastmeier@gmail.com>
2020-02-28 15:03:11 +0100

    Fix missing UTC milliseconds in UTC converted date

--- src/date.ts
+++ src/date.ts
@@ -1,5 +1,5 @@
 export function utc(date: Date = new Date): Date {
-  let utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds())
+  let utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds())
   return new Date(utc)
 }
 
